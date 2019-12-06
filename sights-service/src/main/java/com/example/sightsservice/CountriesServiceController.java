26ff5fe4883408e5097fd154804bda6f62339a54
LucciaChia <krausovalucia23@gmail.com>
2019-12-06 14:27:12 +0100

    unused services deleted

--- sights-service/src/main/java/com/example/sightsservice/CountriesServiceController.java
+++
@@ -1,7 +0,0 @@
-package com.example.sightsservice;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CountriesServiceController {
-}
--- sightservice/src/main/java/com/profesorp/sightservice/SightServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.profesorp.sightservice;
-
-@SpringBootApplication
-public class SightServiceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SightServiceApplication.class, args);
-    }
-}
